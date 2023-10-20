fe014cd625b7b12ffc8a1d01b7a476b8a0797671
Lorenzo Pirritano <lpirritano@anaconda.com>
2023-10-20 23:06:29 +0200

    enable all valid builds, refactor recipe
    - rename patch 002
    - remove script files
    - use inline script

--- recipe/base_build.bat
+++
@@ -1,4 +0,0 @@
-echo "Building %PKG_NAME%"
-
-rem %PYTHON% is not defined
-python -m pip install . --no-deps --no-build-isolation -vv
--- recipe/base_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Building $PKG_NAME"
-
-# $PYTHON is not defined
-python -m pip install . --no-deps --no-build-isolation -vv
